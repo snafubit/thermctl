d550618f52b8a61c88eca3708b16a4dcb5bb298a
SnafuBit <245926328+snafubit@users.noreply.github.com>
2026-02-03 18:55:44 +0100

    Update thermal.h

--- src/thermal.h
+++ src/thermal.h
@@ -1,7 +1,8 @@
-// thermal.h
-// Interface for thermal sensor access
-
 #ifndef THERMAL_H
 #define THERMAL_H
+
+float read_temperature(void);
+int check_threshold(float temp, int threshold);
+void log_temperature(float temp);
 
 #endif
